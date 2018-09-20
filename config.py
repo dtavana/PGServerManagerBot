424f2b47abc6d65dcd041888c0a51ebe41122e0f
TwiSt <datavana@gmail.com>
2018-09-20 12:55:34 +0100

    updated .gitignore

---
+++ config.py
@@ -0,0 +1,20 @@
+#BOT
+token = "NDg3Njg4Nzk5NDA5Nzk5MTY4.DnRTSA.BPVRWJczfezsO9yDSQucCCGKSx8"
+
+#DZ DB
+dzhost = "127.0.0.1"
+dzport = 3306
+dzuser = "root"
+dzpass = "KbBFnTYr"
+dzschema = "pgoverpoch"
+
+#Discord DB
+dishost = "127.0.0.1"
+disport = 3306
+disuser = "root"
+dispass = "KbBFnTYr"
+disschema = "DiscordBot"
+
+#Battlemetrics API
+apitoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjAzODcyYzg5OTFkMTc1NzIiLCJpYXQiOjE1MzczMDcxODIsIm5iZiI6MTUzNzMwNzE4MiwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjozMzUwIn0.dncvNydnUHN15ClMNPUJRaBgLa3JVSb4LiyAw6le3s8'
+
