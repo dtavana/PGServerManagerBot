f135069ae49dd378abeb2df83181b626f9b4bd77
TwiSt <datavana@gmail.com>
2018-11-29 23:02:10 +0000

    added globalitemcheck

--- modules/servermgmt.py
+++ modules/servermgmt.py
@@ -54,6 +54,142 @@ class ServerManagementCog:
         return realsteamid
 
     @commands.command()
+    @commands.has_any_role("Owner", "Developer")
+    async def globalitemcheck(self, ctx, classname: str, minCount: int):
+        try:
+            # Open Connection
+            dzconn = await aiomysql.connect(host=cfg.dzhost, port=cfg.dzport, user=cfg.dzuser, password=cfg.dzpass, db=cfg.dzschema, autocommit=True)
+            dzcur = await dzconn.cursor(aiomysql.DictCursor)
+            disconn = await aiomysql.connect(host=cfg.dishost, port=cfg.disport, user=cfg.disuser, password=cfg.dispass, db=cfg.disschema, autocommit=True)
+            discur = await disconn.cursor(aiomysql.DictCursor)
+
+            notfound = False
+
+            #Object Data
+            await dzcur.execute('SELECT ObjectUID, ObjectID, Worldspace, Inventory, Classname FROM object_data WHERE LOCATE(%s, Inventory) > 0;', (classname,))
+            data = await asyncio.gather(dzcur.fetchall())
+            if not data[0]:
+                notfound = True
+            else:
+                for x in data[0]:
+                    inventory = x['Inventory']
+                    inventory = ast.literal_eval(inventory)
+                    guns = inventory[0]
+                    mags = inventory[1]
+                    backpacks = inventory[2]
+                    worldspace = x['Worldspace']
+                    
+                    startuidIndex = worldspace.find('7656')
+                    playeruid = worldspace[startuidIndex:startuidIndex + 17]
+                    await discur.execute("SELECT DiscordID FROM users WHERE PlayerUID = %s;", (playeruid,))
+                    did = await asyncio.gather(discur.fetchone())
+                    disMemberString = "**Discord User**: Discord Member not found\n"
+                    if(did[0] != None):
+                        did = did[0]['DiscordID']
+                        disMember = ctx.guild.get_member(int(did))
+                        if disMember is not None:
+                            disMemberString = f"**Discord User**: {disMember.mention}\n"
+                    
+                    worldspace = worldspace[1:]
+                    worldspace = worldspace[worldspace.find(
+                        '['):worldspace.find(']') + 1]
+                    ouid = x['ObjectUID']
+                    oid = x['ObjectID']
+                    objectclassname = x['Classname']
+
+                    guns[0] = [x.lower() for x in guns[0]]
+                    mags[0] = [x.lower() for x in mags[0]]
+                    backpacks[0] = [x.lower() for x in backpacks[0]]
+                    newclassname = classname.lower()
+
+                    if newclassname in guns[0]:
+                        index = guns[0].index(newclassname)
+                        count = guns[1][index]
+                    elif newclassname in mags[0]:
+                        index = mags[0].index(newclassname)
+                        count = mags[1][index]
+                    elif newclassname in backpacks[0]:
+                        index = backpacks[0].index(newclassname)
+                        count = backpacks[1][index]
+                    else:
+                        await ctx.send("An error has occured")
+                        return
+                    if count >= minCount:
+                        embed = discord.Embed(
+                            title=f"Success \U00002705", colour=discord.Colour(0x32CD32))
+                        embed.set_footer(text="PGServerManager | TwiSt#2791")
+                        embed.add_field(name=f"Data:", value=f"**Type**: Object\n"
+                                        f"**Classname**: {classname}\n"
+                                        f"**Count**: {count}\n"
+                                        f"**Worldspace**: {worldspace}\n"
+                                        f"**Object Classname**: {objectclassname}\n"
+                                        f"**Object UID**: {ouid}\n"
+                                        f"**Object ID**: {oid}\n"
+                                        f"**PlayerUID**: {playeruid}\n" + disMemberString, inline=False)
+                        await ctx.send(embed=embed)
+            #Garage
+            await dzcur.execute('SELECT PlayerUID, id, DisplayName, Classname, Inventory FROM garage WHERE LOCATE(%s, Inventory) > 0;', (classname,))
+            data = await asyncio.gather(dzcur.fetchall())
+            if not data[0] and notfound:
+                await ctx.send(f"Did not find any objects containing {classname}")
+            else:
+                for x in data[0]:
+                    inventory = x['Inventory']
+                    inventory = ast.literal_eval(inventory)
+                    guns = inventory[0]
+                    mags = inventory[1]
+                    backpacks = inventory[2]
+                    
+                    playeruid = x['PlayerUID']
+                    await discur.execute("SELECT DiscordID FROM users WHERE PlayerUID = %s;", (playeruid,))
+                    did = await asyncio.gather(discur.fetchone())
+                    disMemberString = "**Discord User**: Discord Member not found\n"
+                    if(did[0] != None):
+                        did = did[0]['DiscordID']
+                        disMember = ctx.guild.get_member(int(did))
+                        if disMember is not None:
+                            disMemberString = f"**Discord User**: {disMember.mention}\n"
+                        
+                    gid = x['id']
+                    objectclassname = x['Classname']
+                    objectdisplayname = x['DisplayName']
+
+                    guns[0] = [x.lower() for x in guns[0]]
+                    mags[0] = [x.lower() for x in mags[0]]
+                    backpacks[0] = [x.lower() for x in backpacks[0]]
+                    newclassname = classname.lower()
+
+                    if newclassname in guns[0]:
+                        index = guns[0].index(newclassname)
+                        count = guns[1][index]
+                    elif newclassname in mags[0]:
+                        index = mags[0].index(newclassname)
+                        count = mags[1][index]
+                    elif newclassname in backpacks[0]:
+                        index = backpacks[0].index(newclassname)
+                        count = backpacks[1][index]
+                    else:
+                        await ctx.send("An error has occured")
+                        return
+                    if count >= minCount:
+                        embed = discord.Embed(
+                            title=f"Success \U00002705", colour=discord.Colour(0x32CD32))
+                        embed.set_footer(text="PGServerManager | TwiSt#2791")
+                        embed.add_field(name=f"Data:", value=f"**Type**: Garage\n"
+                                        f"**Item Classname**: {classname}\n"
+                                        f"**Count**: {count}\n"
+                                        f"**Object Displayname**: {objectdisplayname}\n"
+                                        f"**Object Classname**: {objectclassname}\n"
+                                        f"**Garage ID**: {gid}\n"
+                                        f"**PlayerUID**: {playeruid}\n" + disMemberString, inline=False)
+                        await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"{e}")
+        finally:
+            dzconn.close()
+            disconn.close()
+    
+    @commands.command()
     @commands.has_any_role("Owner", "Developer", "Manager", "Head Admin", "Super Admin")
     async def itemcheck(self, ctx, user, classname: str):
         try: 
