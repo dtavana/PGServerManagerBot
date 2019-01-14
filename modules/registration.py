e3607ab0d3a7e1dc0d4f89daa826086b995e9d46
TwiSt <datavana@gmail.com>
2019-01-14 20:37:55 +0000

    upadted to use server ip instead ofdomain

--- modules/registration.py
+++ modules/registration.py
@@ -3,6 +3,7 @@ import discord
 import asyncio
 import aiomysql
 from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
 import traceback
 
 # Misc. Modules
@@ -89,7 +90,7 @@ class RegistrationCog:
             await ctx.send(f"{ctx.author.mention}, an error has occured")
         disconn.close()
     '''
-	
+
     @commands.command()
     @commands.has_any_role("Owner", "Developer", "Manager", "Head Admin", "Super Admin")
     async def adduser(self, ctx, player: discord.Member):
@@ -209,18 +210,19 @@ class RegistrationCog:
             await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         # Close Connection
         disconn.close()
-    
+
     @commands.command()
+    @commands.cooldown(1, 300, BucketType.user)
     async def register(self, ctx):
         try:
             disconn = await aiomysql.connect(host=cfg.dishost, port=cfg.disport, user=cfg.disuser, password=cfg.dispass, db=cfg.disschema, autocommit=True)
             discur = await disconn.cursor(aiomysql.DictCursor)
             if not (await RegistrationCog.check_id(self, ctx.author)):
                 await ctx.author.send("Please visit the following link to log in!")
-                await ctx.author.send(f"http://pgapplications.xyz:3000/auth/steam?id={ctx.author.id}")
+                await ctx.author.send(f"http://144.217.10.151:3000/auth/steam?id={ctx.author.id}")
                 await ctx.author.send("You have one minute to visit this link.")
                 await discur.execute(f"INSERT INTO authqueue (DiscordID, PlayerUID, Status) VALUES ({ctx.author.id}, 0, 0);")
-                endTime = datetime.datetime.now() + datetime.timedelta(minutes=1)
+                endTime = datetime.datetime.now() + datetime.timedelta(minutes=5)
                 while datetime.datetime.now() < endTime:
                     await discur.execute(f"SELECT PlayerUID FROM authqueue WHERE DiscordID = {ctx.author.id} AND Status = 1;")
                     data = await asyncio.gather(discur.fetchone())
@@ -241,6 +243,17 @@ class RegistrationCog:
             print("geh")
         finally:
             disconn.close()
+
+    @register.error
+    async def registration_handler(self, ctx, error):
+        import traceback
+        traceback.print_exception(type(error), error, error.__traceback__)
+        if isinstance(error, commands.CommandOnCooldown):
+            seconds = error.retry_after
+            seconds = round(seconds, 2)
+            hours, remainder = divmod(int(seconds), 3600)
+            minutes, seconds = divmod(remainder, 60)
+            await ctx.send(f"You are on cooldown! Please try again in **{seconds} seconds**")
 
 
 def setup(bot):
