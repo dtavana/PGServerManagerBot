016c42985e9a626935002e8b0fd15b02c34014f4
TwiSt <datavana@gmail.com>
2019-01-14 20:38:57 +0000

    updated ot 5 minutes

--- modules/registration.py
+++ modules/registration.py
@@ -220,7 +220,7 @@ class RegistrationCog:
             if not (await RegistrationCog.check_id(self, ctx.author)):
                 await ctx.author.send("Please visit the following link to log in!")
                 await ctx.author.send(f"http://144.217.10.151:3000/auth/steam?id={ctx.author.id}")
-                await ctx.author.send("You have one minute to visit this link.")
+                await ctx.author.send("You have 5 minutes to visit this link.")
                 await discur.execute(f"INSERT INTO authqueue (DiscordID, PlayerUID, Status) VALUES ({ctx.author.id}, 0, 0);")
                 endTime = datetime.datetime.now() + datetime.timedelta(minutes=5)
                 while datetime.datetime.now() < endTime:
